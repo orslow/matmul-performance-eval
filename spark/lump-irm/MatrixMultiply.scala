4ab6c653c5bf076cedf6a308cb1f94ea01943de6
orslow <41663937+orslow@users.noreply.github.com>
2019-08-23 10:20:18 +0900

    update with some comments

--- spark/lump-irm/MatrixMultiply.scala
+++ spark/lump-irm/MatrixMultiply.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkException
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql._
 
 import org.apache.spark.mllib.linalg.distributed._
@@ -8,43 +9,44 @@ import org.apache.spark.mllib.linalg._
 import org.apache.spark.rdd.RDD
 
 import java.util.NoSuchElementException
-//import scala.collection.mutable._
+import scala.collection.mutable._
 
 import java.io.PrintWriter
 import java.io.File
 import java.io.FileOutputStream
 
-
 object MatrixMultiply extends App {
 
-  val spark = SparkSession
-    .builder()
-    .appName("LumpIRMatrixMultiply")
-    .getOrCreate()
+  def denseMaker(A: List[(Int, Int, Double)], m: Int, k: Int): DenseMatrix = {
+    val values = new Array[Double](m*k)
+    A.foreach( a => values(a._1+a._2*m) = a._3 )
+    new DenseMatrix(m, k, values)
+  }
 
-  import spark.implicits._
+  val conf = new SparkConf().setAppName("LumpIndexedRowMultiply")
+  val sc = new SparkContext(conf)
 
-  val sc = spark.sparkContext
-
-  val p = args(0).toInt
+  val p = args(0).toInt // numPartitions on load file
   val input1 = args(1).toString
   val input2 = args(2).toString
-  val m = args(3).toInt
-  val k = args(4).toInt
-  val n = args(5).toInt
-  val h = args(6).toInt
+  val m = args(3).toInt // M row size
+  val k = args(4).toInt // M col / N row size
+  val n = args(5).toInt // N col size
+  val h = args(6).toInt // split size
+
+  val sqlContext = new SQLContext(sc)
 
   var tik0 = System.nanoTime()
 
   // load each matrix
-  val dataset1 = spark.read.format("com.databricks.spark.csv").option("delimiter", " ").option("header", "false").option("numPartitions", p).option("inferSchema", true).load("hdfs://"+input1)
-  val dataset2 = spark.read.format("com.databricks.spark.csv").option("delimiter", " ").option("header", "false").option("numPartitions", p).option("inferSchema", true).load("hdfs://"+input2)
+  val dataset1 = sqlContext.read.format("com.databricks.spark.csv").option("delimiter", " ").option("header", "false").option("numPartitions", p).option("inferSchema", true).load("hdfs://"+input1)
+  val dataset2 = sqlContext.read.format("com.databricks.spark.csv").option("delimiter", " ").option("header", "false").option("numPartitions", p).option("inferSchema", true).load("hdfs://"+input2)
+
 
   // to RDD
   val rows1: RDD[Row] = dataset1.rdd
   val rows2: RDD[Row] = dataset2.rdd
 
-  // parse
   val matrixEntries1: RDD[MatrixEntry] = rows1.map { case Row(m:Int, k:Int, v:Double) => MatrixEntry(m, k, v) }
 
   val coordMatrix1 = new CoordinateMatrix(matrixEntries1, m, k)
@@ -54,34 +56,36 @@ object MatrixMultiply extends App {
   irMatrix.rows.cache
   irMatrix.rows.take(1)
 
-  val r1 = rows2.map( { case Row(k:Int, n:Int, v:Double) => (k, n, v) } )
+  // group by column
+  val grouped = rows2.map( { case Row(k:Int, n:Int, v:Double) => (k, n, v) } ).groupBy(a => a._2)
+  grouped.cache
+
   
   val jari = Array.fill[Array[Int]](h)(Array.empty[Int])
-  val sk = Array.fill[Array[MatrixEntry]](h)(Array.empty[MatrixEntry])
+  val splits = Array.fill[Array[MatrixEntry]](h)(Array.empty[MatrixEntry])
 
-  val r2 = r1.groupBy(a => a._2)
-
-  r2.cache
-
+  // use % to split columns
   for(i <- 0 to h-1) {
-    //jari(i) = r2.filter(a => java.lang.Math.floorMod(a._1, h) == i ).map( a => a._1 ).collect
-    //sk(i) = r2.filter(a => java.lang.Math.floorMod(a._1, h) == i ).zipWithIndex.map( a => a._1._2.map( b => MatrixEntry(b._1, a._2, b._3) ) ).flatMap( a => a ).collect
-    jari(i) = r2.filter(a => ((a._1 % h) == i) ).map( a => a._1 ).collect
-    sk(i) = r2.filter(a => ((a._1 % h) == i) ).zipWithIndex.map( a => a._1._2.map( b => MatrixEntry(b._1, a._2, b._3) ) ).flatMap( a => a ).collect
+    val ho = h // should be defined
+    jari(i) = grouped.filter(a => ((a._1) % (ho)) == i ).map( a => a._1 ).collect
+    splits(i) = grouped.filter(a => ((a._1) % (ho)) == i ).zipWithIndex.map( a => a._1._2.map( b => MatrixEntry(b._1, a._2, b._3) ) ).flatMap( a => a ).collect
   }
-
-  val sk2 = sk.zipWithIndex
-  sk2.take(1)
+  
+  // zip splits with jari to calculate then locate
+  val hab = splits.zip(jari)
+  hab.take(1)
 
   var tik1 = System.nanoTime()
-  sk2.map( a => irMatrix.multiply(new CoordinateMatrix(sc.parallelize(a._1.toSeq), k, jari(a._2).size).toBlockMatrix.toLocalMatrix ).toCoordinateMatrix.entries.filter( b => b.value != 0.0).map(c => c.i+ " " + jari(a._2)(c.j.toInt) + " " + c.value ) ).map( d => d.saveAsTextFile("/lumpResult/"+d.id) )
+  hab.map( a => irMatrix.multiply(new CoordinateMatrix(sc.parallelize(a._1.toSeq), k, a._2.size).toBlockMatrix.toLocalMatrix ).toCoordinateMatrix.entries.filter( b => b.value != 0.0).map(c => c.i+ " " + a._2(c.j.toInt) + " " + c.value ) ).map( d => d.saveAsTextFile("/lumpResult/"+d.id) )
   var tik2 = System.nanoTime()
 
   val latency1 = ((tik1-tik0) / 1e9)
   val latency2 = ((tik2-tik1) / 1e9)
+  val latency3 = latency1 + latency2
 
   val writer = new PrintWriter(new FileOutputStream(new File("results"),true))
   writer.write("\n" + "Matrix size: " + m + "-" + k + "-" + n + "\n")
   writer.write("[*] Execution time  : " + latency1 + " / " + latency2 + "\n")
+  writer.write("[*] Total time  : " + latency3 + "\n")
   writer.close
 }
